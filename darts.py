1a6aa85aac41b7be82edeb6e6396d02292f2678a
Michael Pfeiffer <michael.pfeiffer@mp76.de>
2014-09-21 20:24:09 +0200

    eine kleine Spieleri, Lesezeichen python: konstruktoren, deep/shallow copy

---
+++ darts.py
@@ -0,0 +1,19 @@
+import sys
+class Game:
+    def __init__(self, params):
+        """ sets the parameters associatable with a dart """
+        self.parameters = params[:]
+
+if __name__ == "__main__":
+    print("configuring game: ")
+    print("hit Enter for just counting | enter some key-value pairs | load config file")
+    #csv
+    g = Game()
+    
+    
+    
+    
+    
+    print("starting game...")
+
+    
